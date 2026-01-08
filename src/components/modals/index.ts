71ba04053994ea6a46da74e92f95bc33c3602632
marlanamc <marlana.creed@gmail.com>
2026-01-08 12:24:45 -0500

    chore: add modal barrel exports

---
+++ src/components/modals/index.ts
@@ -0,0 +1,5 @@
+export { BaseModal } from "./BaseModal";
+export type { ModalOptions, ModalA11yOptions } from "./BaseModal";
+export { eventModal } from "./EventModal";
+export { goalDetailModal } from "./GoalDetailModal";
+export { monthDetailModal } from "./MonthDetailModal";
